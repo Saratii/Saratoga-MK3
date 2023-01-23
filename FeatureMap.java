f55f503e2b84a1c1177b4f97bf3f4bf91b4a6aa3
Your Name <friedchicken14821@gmail.com>
2023-01-23 15:44:17 -0600

    i want to commit domestic terrorism after having to deal with jframe

--- FeatureMap.java
+++ FeatureMap.java
@@ -1,27 +1,33 @@
 import java.awt.image.BufferedImage;
 import javax.swing.*;
+
 import java.awt.*;
 
 public class FeatureMap {
     public FeatureMap(int imageSize, BufferedImage[] images){
-        JFrame frame = new JFrame("UwU");
         JPanel panel = new JPanel();
-        frame.getContentPane();
-        // panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        panel.setBackground(Color.green);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.add(panel);
-        frame.setSize(imageSize * 6, imageSize * 4);
-        frame.setVisible(true);
-        for(int j = 0; j < 3; j++){
-            for(int i = 0; i < 5; i++){
-                // JLabel label = new JLabel(new ImageIcon(images[j * 5 + i]));
-                JLabel label = new JLabel(new ImageIcon(images[0]));
-                label.setBounds(imageSize * i, j * imageSize, imageSize, imageSize);
-                panel.add(label);
-            }
+        GridLayout layout = new GridLayout(3, 5);
+        
+        panel.setLayout(layout);
+        
+        
+        for(BufferedImage image : images){
+            JLabel label = new JLabel(new ImageIcon(image));
+            panel.add(label);
         }
         
+        JFrame frame = new JFrame("uwu");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(imageSize * 5, imageSize * 3);
+        frame.setResizable(false);
+        
+        panel.setBackground(Color.green);
+        
+        frame.add(panel);
+        frame.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0)); 
+
+        panel.setBorder(null);
+        frame.setVisible(true);
     }
 
 }
