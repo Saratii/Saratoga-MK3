7bc56ad6205872be6f7ad6aa329e85f314d45239
Your Name <friedchicken14821@gmail.com>
2023-01-23 14:21:01 -0600

    new version

--- FeatureMap.java
+++ FeatureMap.java
@@ -1,26 +1,27 @@
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.util.Random;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.*;
 import java.awt.*;
 
 public class FeatureMap {
-    private static JFrame frame;
-    public FeatureMap(int size, BufferedImage[] images){
-        frame=new JFrame();
-        frame.setTitle("UwU");
-        frame.setSize(size * 2, size * 2);
+    public FeatureMap(int imageSize, BufferedImage[] images){
+        JFrame frame = new JFrame("UwU");
+        JPanel panel = new JPanel();
+        frame.getContentPane();
+        // panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        panel.setBackground(Color.green);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        for(int i = 0; i < 5; i++){
-            JLabel label = new JLabel(new ImageIcon(images[i]));
-            label.setLocation(i * size * 0, 0);
-            frame.add(label);
-            
-        }
+        frame.add(panel);
+        frame.setSize(imageSize * 6, imageSize * 4);
         frame.setVisible(true);
-
+        for(int j = 0; j < 3; j++){
+            for(int i = 0; i < 5; i++){
+                // JLabel label = new JLabel(new ImageIcon(images[j * 5 + i]));
+                JLabel label = new JLabel(new ImageIcon(images[0]));
+                label.setBounds(imageSize * i, j * imageSize, imageSize, imageSize);
+                panel.add(label);
+            }
+        }
+        
     }
+
 }
