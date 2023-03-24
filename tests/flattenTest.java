93b2db94c38484fc1c94ce21ce35b834aab9da5c
saratii <friedchicken14821@gmail.com>
2023-03-24 12:19:56 -0500

    writing tests for all of eternity

---
+++ tests/flattenTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import src.Flatten;
+import src.Matrix;
+
+public class flattenTest {
+    @Test
+    public void forward(){
+        Matrix a = new Matrix(6, 7, 8);
+        a.seed();
+        Flatten f = new Flatten();
+        Matrix b = f.forward(a);
+        assertEquals(a.size, b.size); 
+        Double sum = 0.0;
+        for(int i = 0; i < a.z; i++){
+            for(int j = 0; j < a.rows * a.cols; j++){
+                sum += a.matrix[i][j];
+            }
+        }
+        assertEquals(sum, Double.valueOf(Matrix.sum(b.matrix[0])));
+
+        Matrix c = new Matrix(2, 3, 4);
+        c.matrix = new Double[][]{{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0},{13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 21.0, 22.0, 23.0, 24.0}};
+        Matrix expected = new Matrix(1, 2 * 3 * 4, 1);
+        expected.matrix = new Double[][]{{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 21.0, 22.0, 23.0, 24.0}};
+        for(int i = 0; i < c.size; i++){
+            assertEquals(f.forward(c).matrix[0][i], expected.matrix[0][i]);
+        }
+    }
+}
---
+++ tests/softMaxTest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import src.Matrix;
+import src.Softmax;
+
+public class softMaxTest {
+    @Test
+    public void forward(){
+        Matrix a = new Matrix(1, 3, 3);
+        a.matrix = new Double[][]{{1.0, 0.99, 2.9, -1.92, 1.1, -0.0001, -0.2, -1.3, 6.0}};
+        Matrix expected = new Matrix(1, 3, 3);
+        Softmax s = new Softmax();
+        Matrix b = s.forward(a);
+        expected.matrix = new Double[][]{{0.00629, 0.00623, 0.042, 0.00034, 0.00695, 0.00231, 0.00189, 0.00063, 0.93331}};
+        for(int i = 0; i < a.size; i++){
+            assertEquals(expected.matrix[0][i], b.matrix[0][i], 0.0001);
+        }
+        assertEquals(Double.valueOf(1.0), Double.valueOf(Matrix.sum(b.matrix[0])), 0.000001);
+        Matrix c = new Matrix(1, 68, 69);
+        c.seed();
+        Matrix d = s.forward(c);
+        Double sum = 0.0;
+        for(int i = 0; i < c.size; i++){
+            sum += Math.exp(c.matrix[0][i]);
+        }
+        for(int i = 0; i < c.size; i++){
+            assertEquals(d.matrix[0][i], Double.valueOf(Math.exp(c.matrix[0][i]) / sum), 0.000001);
+        }
+        assertEquals(Double.valueOf(Matrix.sum(d.matrix[0])), Double.valueOf(1.0), 0.000001);
+    }
+}
