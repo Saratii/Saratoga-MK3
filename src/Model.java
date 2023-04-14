5134e1178d0e446693fa0e4e9fdb6a410cf195ef
Your Name <friedchicken14821@gmail.com>
2023-04-14 00:41:32 -0500

    someone send me latin exorcism

--- src/Model.java
+++ src/Model.java
@@ -8,30 +8,42 @@ public class Model {
     public ArrayList<Layer> layers = new ArrayList<>();
     Matrix[] inputs;
     Matrix[] expected;
-    long startTime;
     Boolean profiling = false;
     public Model(){
         expected = new Matrix[Main.numThreads];
         inputs = new Matrix[Main.numThreads];
     }
     public double forward(Matrix inputs, Matrix expected, int threadIndex) throws Exception{
-        synchronized (this){
-            for(int i = 0; i < layers.size(); i++){
-                startTime = System.currentTimeMillis();
-                inputs = layers.get(i).forward(inputs, threadIndex);
-            }
-        }
+        
+            // for(int i = 0; i < layers.size(); i++){
+            //     inputs = layers.get(i).forward(inputs, threadIndex);
+            // }
+        
+            inputs = layers.get(0).forward(inputs, threadIndex);
+            inputs = layers.get(1).forward(inputs, threadIndex);
+            inputs = layers.get(2).forward(inputs, threadIndex);
+            inputs = layers.get(3).forward(inputs, threadIndex);
+            inputs = layers.get(4).forward(inputs, threadIndex);
+            inputs = layers.get(5).forward(inputs, threadIndex);
+            inputs = layers.get(6).forward(inputs, threadIndex);
+        
+            inputs = layers.get(7).forward(inputs, threadIndex);
+        
+            inputs = layers.get(8).forward(inputs, threadIndex);
+            inputs = layers.get(9).forward(inputs, threadIndex);
+            inputs = layers.get(10).forward(inputs, threadIndex);
+            inputs = layers.get(11).forward(inputs, threadIndex);
+            inputs = layers.get(12).forward(inputs, threadIndex);
+        
         double l = Loss.forward(inputs, expected);
         this.expected[threadIndex] = expected;
         this.inputs[threadIndex] = inputs;
         return l;
-        
     }
     public void backward(int threadIndex){
         Matrix gradients;
         gradients = Loss.backward(inputs[threadIndex], expected[threadIndex]);
         for(int i = 0; i < layers.size(); i++){
-            startTime = System.currentTimeMillis();
             gradients = layers.get(layers.size() - 1 - i).backward(gradients, threadIndex);
         }
     }
