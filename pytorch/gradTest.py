67b5e7022fd6705aa862ffaf466c502e7727b4bb
Your Name <friedchicken14821@gmail.com>
2023-04-16 23:14:31 -0500

    hid folders

--- pytorch/gradTest.py
+++
@@ -1,9 +0,0 @@
-from torch import *
-from torch.nn import *
-
-manual_seed(0)
-set_printoptions(10);
-conv = Conv2d(1, 3, 2, bias=False) #grey scale input, feature sets, kernal size
-input = randn(1, 4, 4)
-print(input)
-print(conv(input))
